6d6af407119f04243daaee6a66531f2629d8e4b7
jili <jili@heidrick.com>
2017-07-27 10:41:00 -0500

    form get, post, file upload, cookies mgmt

--- server.js
+++ server.js
@@ -1,11 +1,84 @@
 var express = require('express');
 var app = express();
+// for form post: requires install
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser')
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+//for file upload
+var fs = require("fs");
+// requires install
+var multer  = require('multer');
+
+
 //http://127.0.0.1:8081/images/logo.png will show the image in the public folder
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// cookies
+app.use(cookieParser());
+
+//for file upload
+//does not work: app.use(multer({ dest: '/tmp/'}));
+app.use(multer({dest:__dirname+'/tmp/'}).any());
+
+//Accessing the HTML document using http://127.0.0.1:8081/index.htm will generate the following form
+app.get('/index.htm', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.htm" );
+})
+
+app.get('/process_get', function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.query.first_name,
+      last_name:req.query.last_name
+   };
+   console.log(response);
+   // redirects to a page with {"first_name":"Jin","last_name":"Li"} on the page
+   res.end(JSON.stringify(response));
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+   // Prepare output in JSON format
+   response = {
+      first_name:req.body.first_name,
+      last_name:req.body.last_name
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+app.post('/file_upload', function (req, res) {
+  //req.files is array, so need to index it
+   console.log(req.files);
+   console.log(req.files[0].filename);
+   console.log(req.files[0].path);
+   console.log(req.files[0].mimetype);
+   var file = __dirname + "/a" + req.files[0].filename;
+   fs.readFile( req.files[0].path, function (err, data) {
+      fs.writeFile(file, data, function (err) {
+         if( err ){
+            console.log( err );
+            }else{
+               response = {
+                  message:'File uploaded successfully',
+                  filename:req.files[0].filename // this is just setting the response, not the actual output file
+               };
+            }
+         console.log( response );
+         res.end( JSON.stringify( response ) );
+      });
+   });
+})
+
 
 // This responds with "Hello World" on the homepage
 app.get('/', function (req, res) {
    console.log("Got a GET request for the homepage");
+   console.log("Cookies: ", req.cookies);
    res.send('Hello GET');
 })
 
